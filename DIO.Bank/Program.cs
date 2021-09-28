83fa77dd933a4ae747e33c4f2f14ae08ec4542db
Mateus Henrique <mateusvillot@gmail.com>
2021-09-28 09:49:44 -0300

    opções do usuário

--- DIO.Bank/Program.cs
+++ DIO.Bank/Program.cs
@@ -1,14 +1,67 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DIO.Bank
 {
     class Program
     {
+        static List<Conta> listContas = new List<Conta>();
         static void Main(string[] args)
         {
-           Conta minhaConta = new Conta(TipoConta.PessoaFisica, 0, 0, "Mateus Santos", "Recife");
+           string opcaoUsuario = ObterOpçaoUsuario();
 
-           Console.WriteLine(minhaConta.ToString());
+           while (opcaoUsuario.ToUpper() != "X")
+           {
+               switch (opcaoUsuario)
+               {
+                   case "1":
+                      //ListarContas();
+                      break;
+                   case "2":
+                      //NovaConta();
+                      break;
+                    case "3":
+                      //Tranferencia();
+                      break;
+                    case "4":
+                      //Saque();
+                      break;
+                    case "5":
+                      //Deposito();
+                      break;
+                    case "C":
+                      Console.Clear();
+                      break;
+                    
+                    default:
+                      throw new ArgumentOutOfRangeException();
+               }
+               
+               opcaoUsuario = ObterOpçaoUsuario();
+           }
+
+           Console.WriteLine("Obrigado por utilizar nossos serviços.");
+           Console.ReadLine();
+        }
+
+        private static string ObterOpçaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Dio Bank a seu Dispor!!");
+            Console.WriteLine("Informe a opção desejada: ");
+
+            Console.WriteLine("1- Listar Contas");
+            Console.WriteLine("2- Nova Conta");
+            Console.WriteLine("3- Transferência");
+            Console.WriteLine("4- Saque");
+            Console.WriteLine("5- Depósito");
+            Console.WriteLine("C- Limpar");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
 
         }
     }
